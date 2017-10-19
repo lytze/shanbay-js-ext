342edd2bc5197d471c92deab6e08ffc47b8392d5
lytze <liyutze.seikiri@gmail.com>
2017-10-19 18:04:26 +0800

    better spelling review performance

--- spelling_quick_review.js
+++ spelling_quick_review.js
@@ -3,72 +3,87 @@ list_of_test = []
 $("body").prepend('\
 	<div id="re-test" class="well"\
 	style="position:fixed; right:50px; top:70px; max-width:400px; z-index:2000; background-color:#eee">\
-		<div class="form-group">\
+		<div id="test-listed" class="list-group"></div>\
+		<div class="form-group" style="position:relative">\
 			<input id="test-a" type="text" class="form-control">\
+			<span id="test-tell-c" style="position:absolute; top:5px; right:7px; color:#209e85; display:none">&#x2714;</span>\
+			<span id="test-tell-r" style="position:absolute; top:5px; right:7px; color:#d2672e; display:none">&#x2718;</span>\
 		</div>\
-		<div id="test-listed"></div>\
 		<div class="btn-group">\
 			<button id="test-check" class="btn">检查</button>\
-			<button id="test-ans" class="btn">提示</button>\
+			<button id="test-hint" class="btn">提示</button>\
 			<button id="test-next" class="btn">下一个</button>\
 		</div>\
 		<hr />\
 		<div class="form-group">\
-			<input id="test-n" type="text" class="form-control" placeholder="单词提示">\
+			<input id="test-h" type="text" class="form-control" placeholder="补充提示">\
 		</div>\
 		<button id="test-add" class="btn">加入队列</button>\
+		<small id="test-tell-o" style="display:none">成功加入</small>\
+		<small id="test-tell-p" style="display:none">找不到单词</small>\
 	</div>\
 ')
 
+function updateWordList() {
+	$("#test-listed").children().remove()
+	$("#test-listed").prepend('<ul class="list-group-item" style="overflow:hidden">' + list_of_test[0][1] + "</ul>")
+	for (i = 1; i < list_of_test.length; i++) {
+		$("#test-listed").prepend('<ul class="list-group-item" style="overflow:hidden; color:#999">' + list_of_test[i][1] + "</ul>")
+	}
+}
+function tellAnswer(what) {
+	if (what == "correct") {
+		$("#test-tell-c").show()
+		$("#test-tell-c").fadeOut()
+	} else {
+		$("#test-tell-r").show()
+		$("#test-tell-r").fadeOut()
+	}
+}
+function tellAdd(what) {
+	if (what == "ok") {
+		$("#test-tell-o").show()
+		$("#test-tell-o").fadeOut()
+	} else {
+		$("#test-tell-p").show()
+		$("#test-tell-p").fadeOut()
+	}
+}
+
 $("#test-add").click(function() {
 	if ($("h1.content").length) {
-		if ($("#test-n").val() != "") {
-			wo = $("h1.content").clone().children().remove().end().text().trim()
-			an = $("#test-n").val()
-			list_of_test.push([wo, an])
-			$("#re-test").append('<small class="alert-fade">成功加入</small>')
-			$(".alert-fade").fadeOut()
-		} else {
-			$("#re-test").append('<small class="alert-fade">提示为空</small>')
-			$(".alert-fade").fadeOut()
-		}
+		wo = $("h1.content").clone().children().remove().end().text().trim()
+		an = $("#review-definitions span.text").text().trim().replace("\n", " | ")
+		hi = $("#test-h").val()
+		list_of_test.push([wo, an, hi, 1])
+		tellAdd("ok")
+		updateWordList()
 	} else {
-		$("#re-test").append('<small class="alert-fade">找不到单词</small>')
-		$(".alert-fade").fadeOut()
+		tellAdd("problem")
 	}
-	$("#test-a").attr("placeholder", list_of_test[0][1])
-	$("#test-listed").children().remove()
-	for (i = 1; i < list_of_test.length; i++) {
-		$("#test-listed").append("<span>" + list_of_test[i][1] + "</span><br/>")
-	}
-	$("#test-n").val("")
+	$("#test-h").val("")
 })
 $("#test-check").click(function() {
 	if ($("#test-a").val() == list_of_test[0][0]) {
-		$("#re-test").prepend('<p class="alert-fade">正确</p>')
-		$(".alert-fade").fadeOut()
-		list_of_test.shift()
-		$("#test-a").attr("placeholder", list_of_test[0][1])
-		$("#test-listed").children().remove()
-		for (i = 1; i < list_of_test.length; i++) {
-			$("#test-listed").append("<span>" + list_of_test[i][1] + "</span><br/>")
-		}
 		$("#test-a").val("")
+		tellAnswer("correct")
+		list_of_test.shift()
+		updateWordList()
 	} else {
-		$("#re-test").prepend('<p class="alert-fade">错误</p>')
-		$(".alert-fade").fadeOut()
+		tellAnswer("wrong")
 	}
 })
-$("#test-ans").click(function() {
-	$("#re-test").prepend('<p class="alert-fade">' + list_of_test[0][0] + '</p>')
-	$(".alert-fade").fadeOut()
+$("#test-hint").click(function() {
+	if (list_of_test[0][2] == "") {
+		$("#test-a").val(list_of_test[0][0].substring(0, list_of_test[0][3]))
+		list_of_test[0][3]++
+	} else {
+		$("#test-listed").append('<ul class="list-group-item" style="overflow:hidden; color:#209e85">' + list_of_test[0][2] + "</ul>")
+		list_of_test[0][2] = ""
+	}
 })
 $("#test-next").click(function() {
 	list_of_test.shift()
-	$("#test-a").attr("placeholder", list_of_test[0][1])
-	$("#test-listed").children().remove()
-	for (i = 1; i < list_of_test.length; i++) {
-		$("#test-listed").append("<span>" + list_of_test[i][1] + "</span><br/>")
-	}
+	updateWordList()
 	$("#test-a").val("")
 })
